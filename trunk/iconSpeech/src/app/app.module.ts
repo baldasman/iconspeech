6f70932142c0a9b18518fd8bd00fbb346ceb2ac3
Joaos92 <jasoares92@gmail.com>
2017-06-24 20:24:01 +0100

    mm

--- trunk/iconSpeech/src/app/app.module.ts
+++ trunk/iconSpeech/src/app/app.module.ts
@@ -14,6 +14,9 @@ import {IconsMessagePage} from "../pages/icons-message/icons-message";
 import {TranslationTextPage} from "../pages/translation-text/translation-text";
 import {TranslationTextResultPage} from "../pages/translation-text-result/translation-text-result";
 import {TranslationVoicePage} from "../pages/translation-voice/translation-voice";
+import { MediaPlugin, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+import { Transfer, FileUploadOptions, TransferObject } from '@ionic-native/transfer';
 import {AppConstants} from "./app.constants";
 import {ServiceLocator} from "../providers/service-locator";
 
@@ -53,7 +56,11 @@ import {ServiceLocator} from "../providers/service-locator";
         StatusBar,
         SplashScreen,
         {provide: ErrorHandler, useClass: IonicErrorHandler},
-        AppConstants
+        AppConstants,
+        MediaPlugin,
+        File,
+        Transfer,
+        {provide: ErrorHandler, useClass: IonicErrorHandler}
     ]
 })
 
