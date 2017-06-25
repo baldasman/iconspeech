dc232658a1eeb41da6dcd0a7b84ca227b3563f51
brunobmartins <brunomart.91@gmail.com>
2017-06-25 01:39:52 +0100

    home

--- trunk/iconSpeech/src/pages/home/home.ts
+++ trunk/iconSpeech/src/pages/home/home.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {BasePage} from "../base-page";
+import {IconsMessagePage} from "../icons-message/icons-message";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage extends BasePage {
 
-  constructor(navCtrl: NavController) {
-    super(navCtrl);
-  }
+    constructor(navCtrl: NavController) {
+        super(navCtrl);
+    }
+
+    goToIconsMessage() {
+        this.navCtrl.push(IconsMessagePage);
+    }
 
 }
