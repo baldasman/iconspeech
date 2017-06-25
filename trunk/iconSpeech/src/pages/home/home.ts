a4a63bb49b56239e803027550f26d0e9cc014ceb
sodacni <sodacni@gmail.com>
2017-06-25 01:55:06 +0100

    qwe

--- trunk/iconSpeech/src/pages/home/home.ts
+++ trunk/iconSpeech/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {BasePage} from "../base-page";
 import {IconsMessagePage} from "../icons-message/icons-message";
+import {TranslationVoicePage} from "../translation-voice/translation-voice";
 
 @Component({
     selector: 'page-home',
@@ -15,6 +16,12 @@ export class HomePage extends BasePage {
 
     goToIconsMessage() {
         this.navCtrl.push(IconsMessagePage);
+    }
+
+    gotToFood(){
+
+      this.navCtrl.push(TranslationVoicePage);
+
     }
 
 }
