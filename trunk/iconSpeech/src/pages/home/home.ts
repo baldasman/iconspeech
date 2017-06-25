8950a02d83b27b6fa877bb8ae53f305c6761b3ad
brunobmartins <brunomart.91@gmail.com>
2017-06-25 02:05:36 +0100

    bue de cenas

--- trunk/iconSpeech/src/pages/home/home.ts
+++ trunk/iconSpeech/src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {BasePage} from "../base-page";
 import {IconsMessagePage} from "../icons-message/icons-message";
+import {CommunicationTypesPage} from "../communication-types/communication-types";
 
 @Component({
     selector: 'page-home',
@@ -15,6 +16,10 @@ export class HomePage extends BasePage {
 
     goToIconsMessage() {
         this.navCtrl.push(IconsMessagePage);
+    }
+
+    goToCommunicationTypes() {
+        this.navCtrl.push(CommunicationTypesPage);
     }
 
 }
--- trunk/iconSpeech/src/pages/icons-message/icons-message.ts
+++ trunk/iconSpeech/src/pages/icons-message/icons-message.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BasePage} from "../base-page";
 
 @Component({
-  selector: 'page-icons-message',
-  templateUrl: 'icons-message.html'
+    selector: 'page-icons-message',
+    templateUrl: 'icons-message.html'
 })
-export class IconsMessagePage {
+export class IconsMessagePage extends BasePage {
 
-  constructor(public navCtrl: NavController) {
+    private messageIcons: string[];
 
-  }
+    constructor(navCtrl: NavController) {
+        super(navCtrl);
+    }
 
+    ionViewDidEnter() {
+        this.messageIcons = [];
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+        this.messageIcons.push('assets/imgs/24h.png');
+    }
+
+    eraseLastIcon() {
+        if (this.messageIcons.length) {
+            this.messageIcons.splice(this.messageIcons.length - 1, 1);
+        }
+    }
 }
